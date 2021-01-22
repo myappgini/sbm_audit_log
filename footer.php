7324a6bed229e9f8c8fac511caab1304d7617e51
landinialejandro <landinialejandro@hotmail.com>
2021-01-22 19:51:16 -0300

    update testing product

--- footer.php
+++ footer.php
@@ -6,9 +6,6 @@
 						<li><a href="http://landini.com.ar">About</a></li>
 					</ul>
 					<p>Designed and built with all the love in the world by <a href="http://landini.com.ar" rel="noopener" target="_blank">Alejandro Landini</a></a>.</p>
-
-					<p>Code licensed <a href="https://github.com/twbs/bootstrap/blob/v3-dev/LICENSE" rel="license noopener" target="_blank">MIT</a>.</p>
-
 				</div>
 			</footer>
 		</div> <!-- /div.container -->
