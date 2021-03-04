714d3fd0f67a5335249fa763281d39cc8ff69ebc
landinialejandro <landinialejandro@hotmail.com>
2021-03-04 10:38:17 -0300

    change to new mwnu audit in admin area

--- app-resources/button.js
+++ app-resources/button.js
@@ -1,23 +1,16 @@
-    doc_ready(function () {
-            const btn = $j('.audit-log')
-            if (btn.length === 0 ){
-                const span = $j('<span />',{
-                    class:"text-warning",
-                    text:"Audit Log"
-                }).prepend('<i class="glyphicon glyphicon-tasks"></i>')
-                const a = $j('<a />',{
-                    class:"navbar-brand audit-log",
-                    href:"auditLog.php"
-                }).append(span)
-        
-                $j('.navbar-right').prepend(a);
-            }
-    });
+doc_ready(function () {
+    const btn = $j('.audit-log')
+    if (btn.length === 0 ){
+        $j.get('dropdown_menu.html', function(res){
+            $j('.navbar-right').prepend(res);
+        });
+    }
+});
 
-    function doc_ready(fn) {
-        if (document.readyState === "complete" || document.readyState === "interactive") {
-            setTimeout(fn, 1);
-        } else {
-            document.addEventListener("DOMContentLoaded", fn);
-        }
-    }    
+function doc_ready(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 2000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}    
